8b784afb625191659001be5e2765968a8065a311
Andrey1985-2020 <kasner.andrey@gmail.com>
2020-11-30 21:21:50 +0200

    Добавлена елочка через массив

--- src/com/academy/lesson02/HomeWorkLesson02/Task09.java
+++ src/com/academy/lesson02/HomeWorkLesson02/Task09.java
@@ -7,13 +7,28 @@ public class Task09 {
         System.out.print("Enter size tree: ");
         Scanner scanner = new Scanner(System.in);
         int sizeTree = scanner.nextInt();
+        System.out.println("Tree through cycle");
         String tree = "*";
 
         for (int i = 0; i < sizeTree; i++) {
             System.out.println(tree);
             tree = tree + "*";
         }
-    }
+// Добавил елочку через массив [][]
+        String[][] treeArray = new String[sizeTree][sizeTree];
+        System.out.println("\nTree through array");
 
+        for (int i = 0; i < treeArray.length; i++){
+            System.out.println();
+            for (int j = 0; j < treeArray.length; j++){
+                if ( j <= i){
+                    treeArray[i][j] = "*";
+                }else {
+                    treeArray[i][j] = " ";
+                }
+                System.out.print(treeArray[i][j]);
+            }
+        }
+    }
 }
 
