b23a38a04fe5fd2c9db0372be4f03d2cce76602a
Andrey1985-2020 <kasner.andrey@gmail.com>
2020-12-04 10:41:19 +0200

    добавил решение без использования массива

--- src/com/academy/lesson02/HomeWorkLesson02/Task08.java
+++ src/com/academy/lesson02/HomeWorkLesson02/Task08.java
@@ -2,6 +2,12 @@ package com.academy.lesson02.HomeWorkLesson02;
 
 import java.util.Scanner;
 
+//Нарисовать квадрат заданного размера. Пример для размера 4:
+//****
+//*  *
+//*  *
+//****
+
 public class Task08 {
     public static void main(String[] args) {
         System.out.print("Enter size square: ");
@@ -22,6 +28,18 @@ public class Task08 {
                 System.out.print(square[i][j]);
             }
         }
+//Решение через циклы без массива
+            for(int i = 0; i < sizeSquare; i++){
+                System.out.println();
+                for(int j = 0; j < sizeSquare; j++){
+                    if( i == 0 || i == sizeSquare - 1 || j == 0 || j == sizeSquare - 1){
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+
 
     }
 }
