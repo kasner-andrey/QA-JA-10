1b3f4e71b1632cf4e191255a78b5bd620bc3358b
Andrey1985-2020 <kasner.andrey@gmail.com>
2020-12-07 13:47:41 +0200

    переименова таск метод

--- src/com/academy/Lesson03/HomeWork/StringAndArray/Task06Main.java
+++ src/com/academy/Lesson03/HomeWork/StringAndArray/Task06Main.java
@@ -4,7 +4,7 @@ package com.academy.Lesson03.HomeWork.StringAndArray;
 //	- выводит исходную строку на экран
 //	- выводит кол-во символов латиницы
 //	- выводит кол-во символов кириллицы
-public class Tsk06Main {
+public class Task06Main {
     //работает только при установке кодировки windows-1251 и класс Task06Main и класс Task06Method должен быть в этой кодировке.
     public static void main(String[] args) {
         Task06Method.CountingNumberOfCyrillicAndLatinCharacters("nnnreлллвкаавлоаоkdhfkhkl 545411 fkjkjdf 87dsлдоват лдлл л");
