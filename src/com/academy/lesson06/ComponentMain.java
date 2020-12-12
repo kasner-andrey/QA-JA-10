ca18bd3b473fd858575287825d5da1755c858cd7
Andrey1985-2020 <kasner.andrey@gmail.com>
2020-12-12 15:37:18 +0200

    constructor

--- src/com/academy/lesson06/ComponentMain.java
+++ src/com/academy/lesson06/ComponentMain.java
@@ -2,10 +2,10 @@ package com.academy.lesson06;
 
 public class ComponentMain {
     public static void main(String[] args) {
-        Button btn = new Button();
+        Button btn = new Button(12, 23, 18, 56, "hello");
         btn.draw();
 
-        CheckBox checkBox = new CheckBox();
+        CheckBox checkBox = new CheckBox(56, 56, 87,12, "Good");
         checkBox.yPosition = 12;
         checkBox.draw();
 
