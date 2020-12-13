4e1c14a55a39cd493e05dd25b787ab69eee696a3
Andrey1985-2020 <kasner.andrey@gmail.com>
2020-12-13 12:54:17 +0200

    z

--- src/com/academy/Lesson03/HomeWork/StringAndArray/Task10Main.java
+++ src/com/academy/Lesson03/HomeWork/StringAndArray/Task10Main.java
@@ -9,7 +9,7 @@ package com.academy.Lesson03.HomeWork.StringAndArray;
 //			Результат:
 //				[support@gmail.com, central.office@mail.com]
 public class Task10Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Task10Method.OpenWidowLine();
     }
 }
