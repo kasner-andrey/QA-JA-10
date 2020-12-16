48edae055940a2c6bb9dfa31bcf887efea44064b
Andrey1985-2020 <kasner.andrey@gmail.com>
2020-12-16 14:56:05 +0200

    Создать массив размера 10, состоящий из классов CustomDate 	- наполнить массив случайными значениями действительных дат 	- вывести все даты массива на консоль 	- вывести только даты месяца февраль 	- *вывести только даты понедельника

--- src/com/academy/lesson04/HomeWorkOOPQvests/ArrayCustomDate.java
+++ src/com/academy/lesson04/HomeWorkOOPQvests/ArrayCustomDate.java
@@ -1,48 +1,101 @@
 package com.academy.lesson04.HomeWorkOOPQvests;
 
+import javax.swing.*;
 import java.text.ParseException;
-import java.util.Scanner;
+import java.util.Locale;
 
-//2) РЎРѕР·РґР°С‚СЊ РјР°СЃСЃРёРІ СЂР°Р·РјРµСЂР° 10, СЃРѕСЃС‚РѕСЏС‰РёР№ РёР· РєР»Р°СЃСЃРѕРІ CustomDate
-//	- РЅР°РїРѕР»РЅРёС‚СЊ РјР°СЃСЃРёРІ СЃР»СѓС‡Р°Р№РЅС‹РјРё Р·РЅР°С‡РµРЅРёСЏРјРё РґРµР№СЃС‚РІРёС‚РµР»СЊРЅС‹С… РґР°С‚
-//	- РІС‹РІРµСЃС‚Рё РІСЃРµ РґР°С‚С‹ РјР°СЃСЃРёРІР° РЅР° РєРѕРЅСЃРѕР»СЊ
-//	- РІС‹РІРµСЃС‚Рё С‚РѕР»СЊРєРѕ РґР°С‚С‹ РјРµСЃСЏС†Р° С„РµРІСЂР°Р»СЊ
-//	- *РІС‹РІРµСЃС‚Рё С‚РѕР»СЊРєРѕ РґР°С‚С‹ РїРѕРЅРµРґРµР»СЊРЅРёРєР°
+//2) Создать массив размера 10, состоящий из классов CustomDate
+//	- наполнить массив случайными значениями действительных дат
+//	- вывести все даты массива на консоль
+//	- вывести только даты месяца февраль
+//	- *вывести только даты понедельника
 public class ArrayCustomDate {
+    public static CustomDate[] arrayDate;
+
     public static void main(String[] args) throws ParseException {
-        Scanner sizeArray = new Scanner(System.in);
-        System.out.println("Р’РІРµРґРёС‚Рµ СЂР°Р·РјРµСЂ РјР°СЃСЃРёРІР°, РєРѕС‚РѕСЂС‹Р№ РЅСѓР¶РЅРѕ СЃРѕР·РґР°С‚СЊ: ");
-        int size = sizeArray.nextInt();
-        System.out.println("Р’РІРµРґРёС‚Рµ РјРµСЃСЏС†, РєРѕС‚РѕСЂС‹Р№ РЅСѓР¶РЅРѕ РѕС‚СЃРѕСЂС‚РёСЂРѕРІР°С‚СЊ: ");
-        int month = sizeArray.nextInt();
-        System.out.println("Р’РІРµРґРёС‚Рµ РґРµРЅСЊ РЅРµРґРµР»Рё, РєРѕС‚РѕСЂС‹Р№ РЅСѓР¶РЅРѕ РѕС‚СЃРѕСЂС‚РёСЂРѕРІР°С‚СЊ: ");
-        String dayWeek = sizeArray.nextLine();
+        int size = Integer.parseInt(JOptionPane.showInputDialog("Введите размер массива рандомных дат"));
 
-
-        CustomDate[] arrayDate = new CustomDate[size];
+        ArrayCustomDate.RandomDate(size);
+        ArrayCustomDate.Printed();
+        ArrayCustomDate.SortMonth();
+        ArrayCustomDate.SortWeek();
+        System.exit(0);
+    }
+//*********************** Создание массива дат ***************
+    public static void RandomDate(int size) {
+        arrayDate = new CustomDate[size];
         int dayCustomDate;
         int monthCustomDate;
         int yearCustomDate;
-
-        for(int i = 0; i < arrayDate.length; i++){
+        for (int i = 0; i < arrayDate.length; i++) {
             monthCustomDate = 1 + (int) (Math.random() * 11);
-            if(monthCustomDate == 4 || monthCustomDate == 6 || monthCustomDate == 9 || monthCustomDate == 11){
+            if (monthCustomDate == 4 || monthCustomDate == 6 || monthCustomDate == 9 || monthCustomDate == 11) {
                 dayCustomDate = 1 + (int) (Math.random() * 29);
-            } else if (monthCustomDate == 2){
+            } else if (monthCustomDate == 2) {
                 dayCustomDate = 1 + (int) (Math.random() * 27);
             } else dayCustomDate = 1 + (int) (Math.random() * 30);
-            yearCustomDate = 1+ (int) (Math.random() * 3000);
+            yearCustomDate = 1990 + (int) (Math.random() * 31);
             arrayDate[i] = new CustomDate(dayCustomDate, monthCustomDate, yearCustomDate);
+//            System.out.println("****** Date " + (i+1) + " ******");
+//            System.out.println(arrayDate[i].getFormattedDate());
         }
-
-
-    //*********************** Р’С‹РІРѕРґ РјР°СЃСЃРёРІР° РґР°С‚ ***************
-    int i = 1;
-        for (CustomDate date: arrayDate) {
-            System.out.println("*** Date " + i + " ***");
+    }
+    //*********************** Вывод массива дат ***************
+    public static void Printed() throws ParseException {
+        System.out.println();
+        System.out.println("++++++++++++++++++++++++++++++++++++");
+        int i = 1;
+        for(CustomDate date:arrayDate){
+            System.out.println("****** Date " + i + " ******");
             System.out.println(date.getFormattedDate());
             i++;
         }
-
     }
+
+    public static void SortMonth() throws ParseException {
+        String month = ArrayCustomDate.WindowMonth();
+        System.out.println();
+        System.out.println("****** Сортировка по выбраному месяцу ******");
+        for (CustomDate date: arrayDate){
+            if(date.getMonth().equals(month)){
+                System.out.println("***** Date *****");
+                System.out.println(date.getFormattedDate());
+            }
+        }
+    }
+
+    public static void SortWeek() throws ParseException {
+        String week = ArrayCustomDate.WindowWeek();
+        System.out.println();
+        System.out.println("****** Сортировка по выбранному дню недели ******");
+        for (CustomDate date: arrayDate){
+            if(date.getFormattedDate().indexOf(week.toLowerCase()) > 0){
+                System.out.println("***** Date *****");
+                System.out.println(date.getFormattedDate());
+            }
+        }
+    }
+
+
+    public static String WindowMonth() {
+        String[] month = {"Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"};
+        String questMonth = (String) JOptionPane.showInputDialog(null, "Выберите месяц для отсева",
+                "Месяц", JOptionPane.QUESTION_MESSAGE, null, month, month[0]);
+        int numberMonth = 0;
+        String numberStringMonth = "1";
+        for(int i = 0; i < month.length; i++){
+            if(month[i].equals(questMonth)){ numberMonth = i + 1;}
+        }
+        if(numberMonth < 10) {
+            numberStringMonth = "0" + numberMonth;
+        }else numberStringMonth = "" + numberMonth;
+        return numberStringMonth;
+    }
+
+    public static String WindowWeek(){
+        String[] weekDay = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+        return (String) JOptionPane.showInputDialog(null, "Выберите день недели для отсева",
+                "Array size", JOptionPane.QUESTION_MESSAGE, null, weekDay, weekDay[0]);
+    }
+
 }
