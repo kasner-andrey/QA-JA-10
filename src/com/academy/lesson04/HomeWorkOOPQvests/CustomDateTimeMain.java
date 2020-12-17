d55fa3ce71d1cf4811f193f3a4432b9563625b9b
Andrey1985-2020 <kasner.andrey@gmail.com>
2020-12-17 10:07:25 +0200

    добавил проверок

--- src/com/academy/lesson04/HomeWorkOOPQvests/CustomDateTimeMain.java
+++ src/com/academy/lesson04/HomeWorkOOPQvests/CustomDateTimeMain.java
@@ -42,37 +42,81 @@ package com.academy.lesson04.HomeWorkOOPQvests;
 //
 //		-* реализовать метод addSeconds(int seconds), который добавляет указанное кол-во секунд к текущей дате
 
+//Протестировать классы Date, DateTime и DateTimeExt:
+//        - все конструкторы
+//        - метод toString()
+//        - метод equals()
+//        - все методы next...()
+//        - validate()
+
 import java.text.ParseException;
 
 public class CustomDateTimeMain {
     public static void main(String[] args) throws ParseException {
-        System.out.println("***************** nextSecond CustomDateTime *******************");
+        System.out.println("***************** nextSecond() CustomDateTime *******************");
         CustomDateTime date = new CustomDateTime(31, 12, 2020, 23, 59, 59);
-        System.out.println(date);
+        System.out.println("toString: " + date);
         date.nextSecond();
-        System.out.println();
-
-        System.out.println("******************** toString CustomDateTime *************************");
         System.out.println(date);
         System.out.println();
 
-        System.out.println("******************** addSeconds CustomDateTime *************************");
+        System.out.println("***************** nextMinute() CustomDateTime *******************");
         CustomDateTime date2 = new CustomDateTime(31, 12, 2020, 23, 59, 59);
-        date2.addSeconds(400000);
+        System.out.println("toString: " + date2);
+        date2.nextMinute();
         System.out.println(date2);
         System.out.println();
 
-        System.out.println("**************** getFormattedDate(boolean isTimeFormat12) *****************************");
-        System.out.println(date2.getFormattedDate(true));
-        System.out.println();
-
-        System.out.println("********************** getFormattedDate() CustomDateTimeExt ***************************");
-        CustomDateTimeExt date3 = new CustomDateTimeExt(31, 12, 2020, 23, 59, 59, 15);
-        System.out.println(date3.getFormattedDate());
-        System.out.println();
-
-        System.out.println("******************** toString CustomDateTimeExt *************************");
+        System.out.println("***************** nextHour() CustomDateTime *******************");
+        CustomDateTime date3 = new CustomDateTime(31, 12, 2020, 23, 59, 59);
+        System.out.println("toString: " + date3);
+        date3.nextHour();
         System.out.println(date3);
+        System.out.println();
+
+        System.out.println("***************** addSeconds() CustomDateTime *************************");
+        CustomDateTime date4 = new CustomDateTime(31, 12, 2020, 23, 59, 59);
+        System.out.println("toString: " + date4);
+        date4.addSeconds(965456589);
+        System.out.println(date4);
+        System.out.println();
+
+        System.out.println("***************** getFormattedDate(boolean isTimeFormat12) *****************************");
+        System.out.println(date4.getFormattedDate(true));
+        System.out.println();
+
+        System.out.println("***************** getFormattedDate() CustomDateTimeExt ***************************");
+        CustomDateTimeExt date5 = new CustomDateTimeExt(31, 12, 2020, 23, 59, 59, 15);
+        System.out.println(date5.getFormattedDate());
+        System.out.println("toString: " + date5);
+        System.out.println();
+
+        System.out.println("***************** equals() CustomDateTimeExt *************************");
+        CustomDateTimeExt date6 = new CustomDateTimeExt(31, 12, 2020, 23, 59, 59, 15);
+        CustomDateTimeExt date12 = new CustomDateTimeExt(31, 12, 2020, 23, 59, 59, 765);
+        System.out.println("equals() true: " + date5.equals(date6));
+        System.out.println("equals() false: " + date12.equals(date6));
+
+
+        System.out.println("***************** equals() CustomDateTime *************************");
+        CustomDateTime date7 = new CustomDateTime(31, 12, 2020, 23, 59, 59);
+        CustomDateTime date8 = new CustomDateTime(31, 12, 2020, 23, 59, 59);
+        System.out.println("equals() true: " + date7.equals(date8));
+        System.out.println("equals() false: " + date8.equals(date4));
+
+        System.out.println("***************** equals() CustomDate *************************");
+        CustomDate date9 = new CustomDate(31, 12, 2020);
+        CustomDate date10 = new CustomDate(31, 12, 2020);
+        CustomDate date11 = new CustomDate(2, 9, 1998);
+        System.out.println("equals() true: " + date9.equals(date10));
+        System.out.println("equals() false: " + date9.equals(date11));
+
+
+
+
+
+
+
 
 
 
