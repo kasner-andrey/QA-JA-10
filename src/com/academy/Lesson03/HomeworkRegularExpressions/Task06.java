895d9c4610ba3d6f0527ede37f574db90d312503
Andrey1985-2020 <kasner.andrey@gmail.com>
2021-01-11 20:26:15 +0200

    6)* Выделить тегом <strong> повторяющиеся слова.
    Примеры:
    	Тhis is a test=> this is a test
    	Тhis is is a test => this is <strong>is</strong> a test

---
+++ src/com/academy/Lesson03/HomeworkRegularExpressions/Task06.java
@@ -0,0 +1,23 @@
+package com.academy.Lesson03.HomeworkRegularExpressions;
+//6)* Выделить тегом <strong> повторяющиеся слова.
+//
+//Примеры:
+//	Тhis is a test=> this is a test
+//	Тhis is is a test => this is <strong>is</strong> a test
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task06 {
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println("********** Task - 6 **********");
+        System.out.println();
+
+        String regex6 = "(\\b[A-Za-z]+\\b)(.*)\\b\\1\\b";
+        String input = "This is 12345 321 is a hello a test not test   ";
+        input = input.replaceAll(regex6, "$1 $2 <strong>$1</strong>");
+        System.out.println(input);
+    }
+}
